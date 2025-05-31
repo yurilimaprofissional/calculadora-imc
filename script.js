ea790af04c88eae33ed0574b4bd36a6ab85e2c31
Yuri Lima <yurilimaprofissional@gmail.com>
2025-05-30 22:15:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,26 +16,21 @@ form.addEventListener('submit', function(event) {
     if (bmi < 18.5) {
         description = 'Cuidado! Você está abaixo do peso!'
     }
-
-    else if (bmi >= 18.5 && bmi <= 25) {
-        description = 'Parabéns! Você está no peso ideal!'
-    }
-
-    else if (bmi > 25 && bmi <= 30) {
-        description = 'Cuidado! Você está com sobrepeso!'
-    }
-
-    else if (bmi > 30 && bmi <= 35) {
-        description = 'Cuidado! Você está com obesidade moderada!'
-    }
-
-    else if (bmi > 35 && bmi <= 40) {
-        description = 'Cuidado! Você está com obesidade severa'
-    }
-
-    else {
-        description = 'Cuidado! Você está com obesidade morbida!'
-    }
+        else if (bmi >= 18.5 && bmi <= 25) {
+            description = 'Parabéns! Você está no peso ideal!'
+        }
+        else if (bmi > 25 && bmi <= 30) {
+            description = 'Cuidado! Você está com sobrepeso!'
+        }
+        else if (bmi > 30 && bmi <= 35) {
+            description = 'Cuidado! Você está com obesidade moderada!'
+        }
+        else if (bmi > 35 && bmi <= 40) {
+            description = 'Cuidado! Você está com obesidade severa'
+        }
+        else {
+            description = 'Cuidado! Você está com obesidade morbida!'
+        }
 
     value.textContent = bmi.replace('.' , ',')
     document.getElementById('description').textContent = description;
