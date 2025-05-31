4cf729ed90153ae2773d7c26fefe09428430dd9d
Yuri Lima <yurilimaprofissional@gmail.com>
2025-05-30 22:39:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,25 +11,33 @@ form.addEventListener('submit', function(event) {
     const value = document.getElementById('value');
     let description ='';
 
+    value.classList.add('attention');
+
     document.getElementById('infos').classList.remove('hidden');
 
     if (bmi < 18.5) {
-        description = 'Cuidado! Você está abaixo do peso!'
+        description = 'Cuidado! Você está abaixo do peso!';
     }
         else if (bmi >= 18.5 && bmi <= 25) {
-            description = 'Parabéns! Você está no peso ideal!'
+            description = 'Parabéns! Você está no peso ideal!';
+            value.classList.add('normal');
+            value.classList.remove('attention');
+            value.classList.remove('sobrepeso');
+            
         }
         else if (bmi > 25 && bmi <= 30) {
-            description = 'Cuidado! Você está com sobrepeso!'
+            description = 'Cuidado! Você está com sobrepeso!';
+            value.classList.remove('attention');
+            value.classList.add('sobrepeso');
         }
         else if (bmi > 30 && bmi <= 35) {
-            description = 'Cuidado! Você está com obesidade moderada!'
+            description = 'Cuidado! Você está com obesidade moderada!';
         }
         else if (bmi > 35 && bmi <= 40) {
-            description = 'Cuidado! Você está com obesidade severa'
+            description = 'Cuidado! Você está com obesidade severa';
         }
         else {
-            description = 'Cuidado! Você está com obesidade morbida!'
+            description = 'Cuidado! Você está com obesidade morbida!';
         }
 
     value.textContent = bmi.replace('.' , ',')
